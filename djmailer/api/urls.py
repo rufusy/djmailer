1afeabb58eaa89a9bfdc4b3554ec959929174f58
rufusy <ruffiam@yahoo.com>
2019-08-30 15:41:31 +0300

    generic views

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import SubscriberView
-from .views import helloWorldView2
 
 
 app_name = 'api'
 
 urlpatterns = [
-    path('subscriber', SubscriberView.as_view(), name='subscriber'),
-    path('hello2', helloWorldView2, name="helloWorld2")
+    path('subscriber', SubscriberView.as_view(), name='subscriber')
 ]
