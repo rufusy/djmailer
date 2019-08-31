52cd873a848e2e343a5d3fe7635be7638f849261
rufusy <ruffiam@yahoo.com>
2019-08-31 16:17:56 +0300

    JWT implementation

--- djmailer/api/urls.py
+++ djmailer/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
 from .views import SubscriberViewSet, login
+from rest_framework_jwt.views import obtain_jwt_token
+
 
 app_name = 'api'
 
@@ -10,6 +12,7 @@ router.register("subscribers", SubscriberViewSet)
 
 urlpatterns = [
     path('login/', login, name="login"),
+    path('jwt-auth/', obtain_jwt_token),
 ]
 
 urlpatterns += router.urls
--- djmailer/djmailer/settings.py
+++ djmailer/djmailer/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
